8fd7306ee1f7f4602ea81555a8f97414be98fb01
JayKun <junkaiong11@gmail.com>
2017-11-19 23:51:38 -0800

    added image files and .h file

--- lab3a.c
+++ lab3a.c
@@ -4,6 +4,41 @@
  * 	Jun Kai Ong
  *	604606304
  *	junkai@g.ucla.edu
- * */
+ * 
+ */
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <getopt.h>
+#include <time.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+int ifd;
 
 
+int main(int argc, char* argv[]){
+	char * img_name = NULL; 
+	if(argc != 2)
+		print_usage_details();
+	
+	img_name = argv[1];
+
+	ifd = open(img_name, O_RDONLY);
+	int errRead = errno;
+
+	if (ifd >= 0) {
+	    close(0);
+	}
+
+	else{
+	    fprintf(stderr, "ERROR opening image file. Failed to open %s\n", img_name);
+	    fprintf(stderr, "%s\n", strerror(errRead));
+	    exit(2);
+	}
+
+	
+}
+	
