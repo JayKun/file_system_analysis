1e4acbfc78183e5aa3addc5b7750dcb9bae776c3
JayKun <junkaiong11@gmail.com>
2017-11-24 20:00:20 -0800

    bugs with primary and double

--- lab3a.c
+++ lab3a.c
@@ -185,26 +185,52 @@ void inode_summary(){
 
 		// Indirect_blocks
 		// Primary
+		
 		if(inode.i_block[12]>0){
 			struct ext2_dir_entry dir_entry;
-			int block_ids [1024 << superblock.s_log_block_size];
-			pread(ifd, &block_ids, sizeof(block_ids), inode.i_block[12]*1024 << superblock.s_log_block_size);
+			int *block_ids = malloc (1024 << superblock.s_log_block_size);
+			pread(ifd, block_ids, 1024 << superblock.s_log_block_size, inode.i_block[12]*(1024 << superblock.s_log_block_size));
 			int k;
 			for(k=0; k<(1024<<superblock.s_log_block_size)/4; k++){
+				fprintf(stderr, "inode_num: %d \n", inode_number);	
 				if(block_ids[k]==0)
 					break;
-				
 				int offset =0;
-				int block_offset = 1024*inode.i_block[12];
 				while(offset < (1024 << superblock.s_log_block_size)){
 					pread(ifd, &dir_entry, sizeof(struct ext2_dir_entry), block_ids[k]*(1024<<superblock.s_log_block_size)+offset);
 					fprintf(stdout, "INDIRECT,%d,%d,%d,%d,%d\n",inode_number, 1, 12+k, inode.i_block[12], block_ids[k]);
 					offset += dir_entry.rec_len;
-					block_offset += dir_entry.rec_len;
 				}
 			}
+			free(block_ids);
 		}
+		
+		// Double
+		if(inode.i_block[13]>0){
+			struct ext2_dir_entry dir_entry;
+			int *block_ids = malloc (1024 << superblock.s_log_block_size);
+			int *d_block_ids = malloc(1024 << superblock.s_log_block_size);
+			pread(ifd, d_block_ids, 1024 << superblock.s_log_block_size, inode.i_block[13]*(1024 << superblock.s_log_block_size));
+			int k;
+			for(k=0; k<(1024<< superblock.s_log_block_size)/4; k++){
+				pread(ifd, block_ids, 1024 << superblock.s_log_block_size, d_block_ids[k]*(1024 << superblock.s_log_block_size));
+				int kk;
+				for(kk=0; kk<(1024<<superblock.s_log_block_size)/4; kk++){
+					if(block_ids[kk]==0)
+						break;
+					int offset = 0;
+					while(offset < (1024 << superblock.s_log_block_size)){
+						pread(ifd, &dir_entry, sizeof(struct ext2_dir_entry), d_block_ids[kk]*(1024<<superblock.s_log_block_size)+offset);
+						fprintf(stdout, "INDIRECT,%d,%d,%d,%d,%d\n",inode_number, 2, 13+kk, inode.i_block[13], d_block_ids[kk]);
+						offset += dir_entry.rec_len;
+					}
+				}
 
+			}
+			free(d_block_ids);
+			free(block_ids);
+			
+		}
 		mode = mode & 0xFFF;	
 		int owner = inode.i_uid;
 		int group = inode.i_gid;
